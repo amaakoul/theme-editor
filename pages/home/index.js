055585a6706067b7da080d7d1fea4093168a5c62
amaakoul <makoul.ahmed@gmail.com>
2021-04-07 00:15:42 +0200

    sync store

---
+++ pages/home/index.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/Home.module.css'
+
+export default function Title(props) {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Welcome to HOME!</h1>
+      </div>
+    </main>
+  )
+}
